83e0b5970e63434570ff60423893a4b8b3d4e4cf
Brayan-Andreys <109758319+Andreys-Gp@users.noreply.github.com>
2024-03-16 20:43:17 -0500

    complement

---
+++ Divergencia/divergencia.js
@@ -0,0 +1,17 @@
+class divergencia {
+    constructor({
+        primera,
+        segunda,
+        tercera
+    }){
+        this.primera = document.getElementById("primera");
+        this.segunda = segunda.getElementById("segunda");
+        this.tercera = tercera.getElementById("tercera");
+    }
+static get listaCoordenas(){
+    return ["COORDENADAD CARTESIANAS","COORDENADAS CILINDRICAS","COORDENADAS ESFERICAS"]
+}
+static coordenadasAceptadas(){
+    return console.info(`las coordenas aceptadas son "${divergencia.listaCoordenas.join(" , ")}"`);
+}
+}
