7417c8988320e8c6a68278c4322abcc66bce3b56
Brayan-Andreys <109758319+Andreys-Gp@users.noreply.github.com>
2024-03-19 22:11:25 -0500

    semillero

--- Divergencia/divergencia.js
+++ Divergencia/divergencia.js
@@ -1,18 +1,46 @@
-class divergencia {
-    constructor({
-        primera,
-        segunda,
-        tercera
-    }){
-        this.primera = document.getElementById("primera");
-        this.segunda = segunda.getElementById("segunda");
-        this.tercera = tercera.getElementById("tercera");
-    }
-static get listaCoordenas(){
-    return ["COORDENADAD CARTESIANAS","COORDENADAS CILINDRICAS","COORDENADAS ESFERICAS"]
-}
-static coordenadasAceptadas(){
-    return console.info(`las coordenas aceptadas son "${divergencia.listaCoordenas.join(" , ")}"`);
-}
 
-}
+form.addEventListener("submit", e => {
+let entrada1=document.getElementById("primaria").value;
+let entrada2=document.getElementById("segunda").value;
+let entrada3=document.getElementById("tercera").value;
+let elementoResultado=document.getElementById("resultado")
+    e.preventDefault();
+    let regex=/^[pPrR@&]+$/;
+
+    if((regex).test(entrada1)){
+        document.getElementById("error2").style.display="inline";
+    }
+    if((regex).test(entrada2)){
+        document.getElementById("error3").style.display="inline";
+    }
+    if((regex).test(entrada3)){
+        document.getElementById("error4").style.display="inline";
+    }
+    if(!(regex).test(entrada1) && !(regex).test(entrada2) && !(regex).test(entrada3)){
+                let cadena1=String(entrada1);
+                let cadena2=String(entrada2);
+                let cadena3=String(entrada3);
+
+                let funcion1=math.parse(cadena1);
+                let funcion2=math.parse(cadena2);
+                let funcion3=math.parse(cadena3);
+
+                let derivada1=math.derivative(funcion1,"x").toString();
+                console.log(derivada1)
+                let derivada2=math.derivative(funcion2,"y").toString();
+                console.log(derivada2)
+                let derivada3=math.derivative(funcion3,"z").toString();
+                console.log(derivada3)
+
+            let textoREsul="<h2>Divergencia</h2>";
+            textoREsul += `(Ax): ${derivada1}<br>`;
+            textoREsul += `(Ay): ${derivada2}<br>`;
+            textoREsul += `(Az): ${derivada3}<br>`;
+
+            elementoResultado.innerHTML=textoREsul;
+            
+
+    }
+ })
+
+
